6e75fdeb660281de4abf8f31fd0c1b2571440455
benmeign <benedicte.meignan@gmail.com>
2017-07-27 12:32:04 +0200

    added signup login logout logic

--- app.js
+++ app.js
@@ -6,10 +6,20 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const expressLayouts     = require('express-ejs-layouts');
 const mongoose           = require('mongoose');
+const passport           = require('passport');
+const session            = require('express-session');
+const MongoStore         = require('connect-mongo')(session);
+const LocalStrategy      = require('passport-local').Strategy;
+const User               = require('./models/user');
+const bcrypt             = require('bcrypt');
 
 mongoose.connect('mongodb://localhost:27017/ironfunds-development');
 
+const index = require('./routes/index');
+const authentication = require('./routes/authentication');
+
 const app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,6 +35,81 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.use(session({
+  secret: 'ironfundingdev',
+  resave: false,
+  saveUninitialized: true,
+  store: new MongoStore( { mongooseConnection: mongoose.connection })
+}))
+
+// NEW
+passport.serializeUser((user, cb) => {
+  cb(null, user.id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+// Signing Up
+passport.use('local-signup', new LocalStrategy(
+  { passReqToCallback: true },
+  (req, username, password, next) => {
+    // To avoid race conditions
+    process.nextTick(() => {
+        User.findOne({
+            'username': username
+        }, (err, user) => {
+            if (err){ return next(err); }
+
+            if (user) {
+                return next(null, false);
+            } else {
+                // Destructure the body
+                const { username, email, description, password } = req.body;
+                const hashPass = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+                const newUser = new User({
+                  username,
+                  email,
+                  description,
+                  password: hashPass
+                });
+
+                newUser.save((err) => {
+                    if (err){ next(err); }
+                    return next(null, newUser);
+                });
+            }
+        });
+    });
+}));
+
+passport.use('local-login', new LocalStrategy((username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
+
+    return next(null, user);
+  });
+}));
+
+// NEW
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 app.use( (req, res, next) => {
   if (typeof(req.user) !== "undefined"){
     res.locals.userSignedIn = true;
@@ -34,9 +119,8 @@ app.use( (req, res, next) => {
   next();
 });
 
-const index = require('./routes/index');
 app.use('/', index);
-
+app.use('/', authentication);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
